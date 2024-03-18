638599208cdac575188fb7439e26a68d23fbcad9
tomas-B17 <tomaspial95@gmail.com>
2024-03-18 20:46:06 +0100

    Hola mundo
    es mi primer progrma en .net

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Hola Mundo!");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
+
+
+
